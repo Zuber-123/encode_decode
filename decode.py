6803bda0231b6d7d3355cd65594c8bc3983b3bcf
Zuber khan <64692526+Zuber-123@users.noreply.github.com>
2022-08-31 20:12:06 +0530

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,35 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type encode to 'encrypt' or decode to 'decrypt':\n")
+text = input("type the message:\n").lower()
+shift = int(input("type the number shift\n"))
+
+# This is the code for encription
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    new_letter = alphabet[new_position]
+    cipher_text += new_letter
+  print(f"the encoded text is {cipher_text} ")
+
+
+
+
+
+# This is the code for decryption
+
+def decrypt(cipher_text, shift_amount):
+  plain_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    plain_text += alphabet[new_position]
+  print(f"the decoded text is {plain_text} ")
+
+
+if direction == "encode":
+  encrypt(plain_text = text, shift_amount = shift)
+elif direction == "decode":
+  decrypt(cipher_text = text, shift_amount = shift)
